151ad07637a2ac5f0b6b2592143c145242000e0b
gremvanek <gremvanek@gmail.com>
2024-10-26 00:03:56 +0500

    Добавил urls модели Item. Добавлена функция для логов

--- tz_app/app/views.py
+++ tz_app/app/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 
+import os
 from tz_app.app.models import Item
 from tz_app.app.serializers import ItemSerializer
+from tz_app.tz_app import settings
 
 
+def log_to_file(action, item_data):
+    log_file_path = os.path.join(settings.BASE_DIR, 'changes_log.txt')
+    with open(log_file_path, 'a') as f:
+        f.write(f'{action}:{item_data}\n')
+
+        
 class ItemViewSet(ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
